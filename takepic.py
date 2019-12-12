192420b2d3d0bd5a9e323a4e25b84f931e456301
ponjiro <tknrizn@gmail.com>
2019-12-12 13:14:48 +0000

    [modify] insert preview before start

--- takepic.py
+++ takepic.py
@@ -15,18 +15,21 @@ GPIO.setmode(GPIO.BCM)
 gp_out = 2
 GPIO.setup(gp_out,GPIO.OUT)
 
-motor = GPIO.PWM(gp_out,40)
+motor = GPIO.PWM(gp_out,50)
 motor.start(1.0)
-resolution_setting = (1024,768)
+resolution_setting = (250,250)
 
-time_sleep = 1
+time_sleep =2
 
 
 try:	
 	for i in range(2,12):
 		with picamera.PiCamera() as camera:
 			camera.resolution = resolution_setting
-			#camera.start_preview()
+			if i == 2:
+				camera.start_preview()
+				val = input('enter key for start')
+				
 			time.sleep(time_sleep)
 			print("start {}".format(i))
 			camera.capture("output/picture_{}.jpg".format(i))
